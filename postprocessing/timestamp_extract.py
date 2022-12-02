e6ef227caa50c2418e21088347889f057b3e04e2
aib_Desktop <ah444mad@hotmail.com>
2022-12-02 20:30:31 +0300

    Added a column to indicate a frame drop

--- postprocessing/timestamp_extract.py
+++ postprocessing/timestamp_extract.py
@@ -4,64 +4,69 @@ import pyzed.sl as sl
 from tkinter import *
 
 
-
 def main():
-    # Arg Parsing
-    if len(sys.argv) != 2:
-        print("Please specify path to .svo file.")
-        exit()
-    filepath = sys.argv[1]
-    print("Reading SVO file: {0}".format(filepath))
+	# Arg Parsing
+	if len(sys.argv) < 2:
+		print("Please specify path to .svo file.")
+		exit()
+	filepaths = []
+	for i in range(len(sys.argv)-1):
+		filepaths.append(sys.argv[i+1])
 
-    # Init camera
-    input_type = sl.InputType()
-    input_type.set_from_svo_file(filepath)
-    init = sl.InitParameters(input_t=input_type, svo_real_time_mode=False)
-    cam = sl.Camera()
-    status = cam.open(init)
-    if status != sl.ERROR_CODE.SUCCESS:
-        print(repr(status))
-        exit()
+	# Init camera
+	input_type = sl.InputType()
 
-    # Runtime parameters
-    runtime = sl.RuntimeParameters(enable_depth=False)
+	for filepath in filepaths:
+		print("Reading SVO file: {0}".format(filepath))
+		input_type.set_from_svo_file(filepath)
+		init = sl.InitParameters(input_t=input_type, svo_real_time_mode=False)
+		cam = sl.Camera()
+		status = cam.open(init)
+		if status != sl.ERROR_CODE.SUCCESS:
+			print(repr(status))
+			exit()
 
-    # List to hold every {frame #, timestamp}
-    out_data = []
+		# Runtime parameters
+		runtime = sl.RuntimeParameters(enable_depth=False)
 
-    while True:
-        # Grab a frame
-        err = cam.grab(runtime)
+		# List to hold every {frame #, timestamp}
+		out_data = []
+		dropped_frames = 0	
 
-        # Successful grab
-        if err == sl.ERROR_CODE.SUCCESS:
-            # Get timestamp
-            timestamp = cam.get_timestamp(sl.TIME_REFERENCE.IMAGE).get_milliseconds()
-            # Get frame number
-            fn = cam.get_svo_position()
-            # Record timestamp in list
-            out_data.append({'#': fn, 'timestamp': timestamp})
+		while True:
+			# Grab a frame
+			err = cam.grab(runtime)
 
-            if fn % 1000 == 0:
-                print(fn)
+			# Successful grab
+			if err == sl.ERROR_CODE.SUCCESS:
+				# Get timestamp
+				timestamp = cam.get_timestamp(sl.TIME_REFERENCE.IMAGE).get_milliseconds()
+				# Get frame number
+				fn = cam.get_svo_position()
+				# Record if there was dropped frames before this one and how many are dropped
+				dropped = cam.get_frame_dropped_count()-dropped_frames
+				dropped_frames+=dropped
+				# Record timestamp in list
+				out_data.append({'#': fn, 'timestamp': timestamp, 'dropped frames': dropped})
 
-        # End of file reached
-        elif err == sl.ERROR_CODE.END_OF_SVOFILE_REACHED: 
-            break
+				if fn % 1000 == 0:
+					print(fn)
 
-    # Closing camera
-    cam.close()
-    
-    # Write csv file
-    with open(filepath.replace('.svo', '_timestamps.csv'), 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames = ['#', 'timestamp'])
-        writer.writeheader()
-        writer.writerows(out_data)
-    
+			# End of file reached
+			elif err == sl.ERROR_CODE.END_OF_SVOFILE_REACHED:
+				break
 
+		# Closing camera
+		cam.close()
 
-    print("\nFINISH")
+		# Write csv file
+		with open(filepath.replace('.svo', '_timestamps.csv'), 'w') as csvfile:
+			writer = csv.DictWriter(csvfile, fieldnames=['#', 'timestamp', 'dropped frames'])
+			writer.writeheader()
+			writer.writerows(out_data)
+
+	print("\nFINISH")
 
 
 if __name__ == "__main__":
-    main()
+	main()
