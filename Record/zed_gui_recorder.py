0b20c953a7fd410be967874fbeab20d1a1104186
Ahmad Batwa <ah444mad@hotmail.com>
2023-05-03 23:13:31 +0300

    Now Fully functional with 2 active users at once
    Now, if one user uses his start button, recording starts,
    if another user comes while the first one is still active,
    he will be able to use the other button, and the script will
    record his arrival time.
    
    The recording stops only when no more active users present.
    And it starts only if there are no active users and someone press start.
    
    Metadata is recorded for every user: ID, st-time, end-time, position.
    This metadata is written alongside the video whenever recording stops.
    
    Functions like start_recording and stop_recording were modified to
    return descriptive values to the calling function.
    the calling function now is the use_action function, which is
    called whenever a user uses start/stop button. this function handles
    everything, and is responsible to call other functions to perform
    actions.
    
    WIP: remove redundent buttons and add flagging button to each user.

--- Record/zed_gui_recorder.py
+++ Record/zed_gui_recorder.py
@@ -52,7 +52,7 @@ def on_close():
 
 
 # ========= Recording Function ========= #
-def rec_loop(status, runtime_params, rec_path, usrid, fps):
+def rec_loop(status, runtime_params, rec_path, usrid, dt):
 	"""
 		This function manages video recording. 
 		
@@ -71,8 +71,6 @@ def rec_loop(status, runtime_params, rec_path, usrid, fps):
 			Recording directory. must exists.
 		usrid: int
 			user id provided from GUI to use in file name.
-		fps: int
-			camera FPS
 	"""
 
 	global zed 			 # Camera object
@@ -80,10 +78,8 @@ def rec_loop(status, runtime_params, rec_path, usrid, fps):
 	global aborted  	 # Recording abortion indicator
 
 	print("\n[INFO] SETTING RECORDING PARAMETERS")
-	# Current date & time
-	dt=datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")  
 	# Recording path
-	rec_path = os.path.join(rec_path, str(usrid.get())+"_"+dt+'.svo')  
+	rec_path = os.path.join(rec_path, dt+'.svo')  
 	print("[INFO] RECORDING TO: ", rec_path) 
 	# Recording Parameters
 	rec_params = sl.RecordingParameters(rec_path, sl.SVO_COMPRESSION_MODE.H265)
@@ -118,6 +114,7 @@ def main():
 	global is_recording  # Recording condition (Set false to stop any thread recording)
 	global aborted  	 # Recording abortion indicator
 
+	# ========================= setup Section ========================= #
 	# Set default rec_path
 	# rec_path = r"/home/felemban/Documents"
 	# rec_path = r"/media/felemban/Extreme SSD"
@@ -140,11 +137,17 @@ def main():
 			camera_resolution=sl.RESOLUTION.HD720,
 			camera_fps=60,
 			depth_mode=sl.DEPTH_MODE.NONE)
-		
+	
 	# Runtime Parameter
 	print("\n[INFO] SETTING RUNTIME PARAMETERS")
 	runtime_params = sl.RuntimeParameters(enable_depth=False)
 
+	# Metadata structure initialization
+	metadata_fname = ''
+	metadata = [{"ID": [], "Position": [], "StartTime": [],"EndTime": []},
+	      		{"ID": [], "Position": [], "StartTime": [],"EndTime": []}]
+	
+	
 	# ========================= TK GUI Section ========================= #
 	root = tk.Tk()
 	status = tk.StringVar()			 # Store the status of recording
@@ -154,11 +157,13 @@ def main():
 	fps=tk.IntVar()					 # Store current FPS
 	fps.set(0)						 # initially set to 0
 	
-	usr_btn_stat = [tk.StringVar(), tk.StringVar()]
-	[btn_stat.set('Start') for btn_stat in usr_btn_stat]
+	# The same button is used for start/stop. So, to iterate the text,
+	# ... the test is initialized as tk.StringVar() which changes in rt.
+	usr_btn_stat = [tk.StringVar(), tk.StringVar()]  		# Start/Stop buttons' texts holder
+	[btn_stat.set('Start') for btn_stat in usr_btn_stat]	# Initialized to Start
 
-	active_usrs = tk.IntVar()
-	active_usrs.set(0)
+	active_usrs = tk.IntVar()	# Store number of active users (0, 1, 2)
+	active_usrs.set(0)			# Initialize to 0
 
 	th=None  			# Thread object holder
 	recstat_lbl=None	# Recording status label object holder. 
@@ -167,22 +172,19 @@ def main():
 						# the start_recording function
 
 
-	# Action item for start recording button
+	# Action item for start recording
 	def start_recording():
 		global zed
 		global is_recording
+		nonlocal metadata
+		nonlocal metadata_fname
 
-		# If recording is already started
+		# If recording is already started (there is already an active user using the interface)
+		# ... then no need to start the recording again, as it is already running.
 		if is_recording:
 			print("Recordings Locked!")
-			return
-		
-		# # If user didn't provide an id
-		# if usrid.get() < 0:
-		# 	print("Please provide correcut user id")
-		# 	messagebox.showerror("Error", "Please provide correcut user id")
-		# 	return
-		
+			# Returning true indicates that the recording is running
+			return True
 		# If the camera is not open, then open it.
 		if not zed.is_opened():
 			print("Opening camera")
@@ -190,15 +192,20 @@ def main():
 			if st != sl.ERROR_CODE.SUCCESS:
 				print(st)
 				return False
-		
 		# Set recording flag to True
 		is_recording = True
 		# Update status label color
-		recstat_lbl.configure(bg="#25be46")  
+		recstat_lbl.configure(bg="#25be46")
+		# Current date & time
+		dt=datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
 		# Start the recording thread
-		th=threading.Thread(target=rec_loop, args=(status,runtime_params,rec_path,usrid,fps))
+		th=threading.Thread(target=rec_loop, args=(status,runtime_params,rec_path,usrid,dt))
 		th.start()
-		
+		# Set the file name of the output file to the time at which the recording started at
+		metadata_fname = dt
+		# Returning true indicates that the recording is running
+		return True
+
 
 	# Action item for stop recording button
 	def stop_recording():
@@ -207,12 +214,14 @@ def main():
 		# If recording is already stopped
 		if is_recording == False:
 			print("Recording already stopped")
+			messagebox.showerror("Error", "Recording already stopped!")
 			return
 		
 		# Check if there is still a user using the interface
 		if active_usrs.get() != 0:
-			print("A user is still engaged. Wait for them.")
-			return
+			print("A user is still engaged. Waiting for them.")
+			return datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
+			# return zed.get_svo_number_of_frames
 		
 		# Reset the recording flag
 		is_recording = False
@@ -222,12 +231,11 @@ def main():
 		try:
 			th.join()
 		except:
-			# fps.set(0)
 			# Update status label
 			recstat_lbl.configure(bg="#de5e5e")  
 			# Close the camera
 			zed.close()
-			return
+			return datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
 
 
 	# Stop record and delete the recorded files
@@ -242,8 +250,12 @@ def main():
 	# Read User id input
 	def usr_action(id_ent, usr):
 		global zed
+		nonlocal metadata
+		nonlocal metadata_fname
 		
+		# Get the ID of the current user
 		ID = id_ent.get()
+		# Check if an ID is provided and if is integer
 		try:
 			usrid.set(int(ID))
 			if usrid.get() < 0: raise ValueError
@@ -252,16 +264,57 @@ def main():
 			messagebox.showerror("Error", "Please provide correct user id")
 			return
 		
+		# If the user pressed start
 		if usr_btn_stat[usr].get() == "Start":
+			# Update the button text to stop (and its functionality changes with it)
 			usr_btn_stat[usr].set("Stop")
-			if start_recording() != False:  # Camera opened successfully
-				active_usrs.set(active_usrs.get() + 1)
-			
-		elif usr_btn_stat[usr].get() == "Stop":
-			usr_btn_stat[usr].set("Start")
-			active_usrs.set(active_usrs.get() - 1)
-			stop_recording()
+			# Check if camera opened successfully
+			if start_recording() == False: 
+				messagebox.showerror("ERROR", "Camera Failed To Open")
+				return
+			# Set StartTime for this user in metadata
+			# If this is the a new recording:
+			if active_usrs.get() == 0:
+				# Then starting time is same as recording start time
+				metadata[usr]["StartTime"].append(metadata_fname)
+			# If not new recording (previous user started recording and current user joined later):
+			else: 
+				# Then start time is now.
+				metadata[usr]["StartTime"].append(datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p"))
+			# Set other parts of metadata
+			metadata[usr]["ID"].append(usrid.get())
+			metadata[usr]["Position"].append(usr)
+			# Increment number of active users.
+			active_usrs.set(active_usrs.get() + 1)
 		
+		# If the user pressed stop
+		elif usr_btn_stat[usr].get() == "Stop":
+			# Update the button text to start (and its functionality changes with it)
+			usr_btn_stat[usr].set("Start")
+			# Decrement active users
+			active_usrs.set(active_usrs.get() - 1)
+			# update metadata
+			metadata[usr]["EndTime"].append(stop_recording())
+			# Preparing and writing metadata output
+			output_dict = {}
+			# Write the metacata file only if there are no active users
+			if active_usrs.get() == 0:  
+				# The metadata dictionary has two lists, one for each user position
+				# Each position can contain more than one datapoint (more than one user)
+				# Merge the two lists into one dictionary
+				for k in metadata[0].keys():
+					output_dict[k] = metadata[0][k] + metadata[1][k]
+				# Write metadata to a csv file
+				with open(os.path.join(rec_path, metadata_fname)+'.csv', 'w', newline='') as output_file:
+					writer = csv.writer(output_file)
+					writer.writerow(list(output_dict.keys()))
+					writer.writerows(zip(*output_dict.values()))
+
+				# Reset metadata for next recording
+				metadata = [{"ID": [], "Position": [], "StartTime": [],"EndTime": []},
+							{"ID": [], "Position": [], "StartTime": [],"EndTime": []}]
+				metadata_fname = ''
+				
 
 	# Creating Main Frame
 	frm_main = tk.Frame(master=root, width = 500, height=500)
@@ -290,14 +343,14 @@ def main():
 	conf_curid_lbl = tk.Label(master=frm_main, textvariable= usrid, font=('Times New Roman', 14))
 	conf_curid_lbl.grid(row=2, column=1, columnspan=3, padx=(15,15), pady=(15,15), sticky="NSW")
 
-	ctrl_lbl = tk.Label(master=frm_main, text="Controls: ", font=('Times New Roman', 14))
-	ctrl_lbl.grid(row=3, column=0, padx=(15,0), sticky="NSE")
-	start_btn = tk.Button(master=frm_main, text="Start Recording", font=('Times New Roman', 14), command=start_recording)
-	start_btn.grid(row=3, column=1, padx=(15,0), sticky='EW')
-	stop_btn = tk.Button(master=frm_main, text="Stop Recording", font=('Times New Roman', 14), command=stop_recording)
-	stop_btn.grid(row=3, column=2,padx=(0,0), sticky='EW')
-	abort_btn = tk.Button(master=frm_main, text="Abort Recording", font=('Times New Roman', 14), command=abort_recording)
-	abort_btn.grid(row=3, column=3,padx=(0,15), sticky='EW')
+	# ctrl_lbl = tk.Label(master=frm_main, text="Controls: ", font=('Times New Roman', 14))
+	# ctrl_lbl.grid(row=3, column=0, padx=(15,0), sticky="NSE")
+	# start_btn = tk.Button(master=frm_main, text="Start Recording", font=('Times New Roman', 14), command=start_recording)
+	# start_btn.grid(row=3, column=1, padx=(15,0), sticky='EW')
+	# stop_btn = tk.Button(master=frm_main, text="Stop Recording", font=('Times New Roman', 14), command=stop_recording)
+	# stop_btn.grid(row=3, column=2,padx=(0,0), sticky='EW')
+	# abort_btn = tk.Button(master=frm_main, text="Abort Recording", font=('Times New Roman', 14), command=abort_recording)
+	# abort_btn.grid(row=3, column=3,padx=(0,15), sticky='EW')
 
 	status_lbl = tk.Label(master=frm_main, text="Status", font=('Times New Roman', 14))
 	status_lbl.grid(row=4, column=0, padx=(15,0), pady=(15,15), sticky="NSE")
